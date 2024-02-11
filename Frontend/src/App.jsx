f2d4972617e4d72058a373818c0f2c30911b741a
sudheer0071 <sudheer1614@gmail.com>
2024-02-11 23:32:26 +0530

    .

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import React from 'react';
 import Modal from 'react-modal'
-// import reactLogo from './assets/react.svg'
+import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { CreateTodo } from './compnents/CreateTodo'
