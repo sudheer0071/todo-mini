2cf32456450b9d19c9e4a8f12381d7286260050d
sudheer0071 <sudheer1614@gmail.com>
2024-02-21 23:34:01 +0530

    .

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -88,7 +88,7 @@ function App() {
         <div className="right-bar">
           <h2>Todos list</h2>
           <div className="todolist">
-          <h2 className='notodos'>{todolist.length==0?"No Todos to show":''} <br />{todolist.length==0 && <svg width="44" height="34" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M12 11.5V16.5" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" /> <path d="M12 7.51L12.01 7.49889" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" /> <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" /> </svg>}</h2>
+            <h2 className='notodos'>{todolist.length == 0 ? "No Todos to show" : ''} <br />{todolist.length == 0 && <svg width="44" height="34" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M12 11.5V16.5" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" /> <path d="M12 7.51L12.01 7.49889" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" /> <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" /> </svg>}</h2>
             <ul>{todolist}</ul>
           </div>
         </div>
@@ -117,7 +117,7 @@ function App() {
           {showmobileTodoList && (
             <div className={`blur-wrapper ${showmobileTodoList ? 'show' : ''}`}>
               <div className="todolist">
-                <h2 className='notodos'>{todolist.length == 0 ? "No Todos to show" : ''} <br />{todolist.length==0 && <svg width="44" height="34" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M12 11.5V16.5" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" /> <path d="M12 7.51L12.01 7.49889" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" /> <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" /> </svg>}</h2>
+                <h2 className='notodos'>{todolist.length == 0 ? "No Todos to show" : ''} <br />{todolist.length == 0 && <svg width="44" height="34" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M12 11.5V16.5" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" /> <path d="M12 7.51L12.01 7.49889" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" /> <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" /> </svg>}</h2>
 
                 <ul>{todolist}</ul>
               </div>
